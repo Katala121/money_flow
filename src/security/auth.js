002541bbe501bc1e34c8e76ec99513ed7ffe9b44
Katala121 <sanyafootb@list.ru>
2020-04-21 13:03:25 +0300

    simple commit

--- src/security/auth.js
+++ src/security/auth.js
@@ -8,17 +8,13 @@ async function checkUser(request, response, next) {
     try {
         if (request.headers.authorization !== undefined) {
             const token = request.headers.authorization.split(' ')[1];
-    
             const decoded = await jwt.decode(token);
-    
             const user = await userRepository.findByEmailAndId({
                 id: decoded.id,
                 email: decoded.email,
             });
-    
             if (user !== null && await jwt.verify(token, user.password) !== null) {
                 request.user = user;
-    
                 next();
             } else {
                 next(new Error('Invalid auth data'));
@@ -27,9 +23,7 @@ async function checkUser(request, response, next) {
             next(new Error('Invalid auth data'));
         }
     } catch (e) {
-        next(new Error('Invalid auth data'))
+        next(new Error('Invalid auth data'));
     }
-    
 }
-
 export default checkUser;
